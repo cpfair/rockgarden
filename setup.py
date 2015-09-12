c901ec680ebc7f231aed5a0200119a987ca5e999
Collin Fair <cpf@cpfx.ca>
2015-09-12 13:30:16 -0400

    Packageify

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Rock Garden',
+      version='0.1',
+      description='Patch Pebble apps',
+      author='Collin Fair',
+      url='https://github.com/cpfair/rockgarden',
+      packages=['rockgarden'],
+     )
